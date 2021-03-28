b090e4d96da73ac4793cbb64801d0ca10253707d
JSA <jet2303@naver.com>
2021-03-28 18:37:43 +0900

    스프링부트프로젝트(어드민페이지만들기)_14JPA연관관계설정배우기_코딩하기_2

--- src/test/java/com/example/study/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/study/repository/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.study.repository;
 
+
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.Item;
 import org.junit.jupiter.api.Assertions;
@@ -9,22 +10,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 
 public class ItemRepositoryTest extends StudyApplicationTests {
+
     @Autowired
     private ItemRepository itemRepository;
-
 
     @Test
     public void create(){
         Item item = new Item();
+
         item.setName("노트북");
         item.setPrice(100000);
         item.setContent("삼성 노트북");
-        item.setTitle("title");
-        item.setPartnerId(1L);
 
         Item newItem = itemRepository.save(item);
         Assertions.assertNotNull(newItem);
-
     }
 
     @Test
@@ -32,6 +31,10 @@ public class ItemRepositoryTest extends StudyApplicationTests {
         Long id = 1L;
 
         Optional<Item> item = itemRepository.findById(id);
+
         Assertions.assertTrue(item.isPresent());
+        item.ifPresent(i ->{
+            System.out.println(i);
+        });
     }
 }
--- src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.example.study.repository;
 
+
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.OrderDetail;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import sun.jvm.hotspot.utilities.Assert;
 
 import java.time.LocalDateTime;
 
-
+@Repository
 public class OrderDetailRepositoryTest extends StudyApplicationTests {
+
     @Autowired
     private OrderDetailRepository orderDetailRepository;
 
@@ -18,19 +21,16 @@ public class OrderDetailRepositoryTest extends StudyApplicationTests {
     public void create(){
         OrderDetail orderDetail = new OrderDetail();
 
+        orderDetail.setOrderAt(LocalDateTime.now());
+        //어떤 아이템
+        orderDetail.setItemId(7L);
 
-        orderDetail.setStatus("status1");
-        orderDetail.setArrivalDate(LocalDateTime.now());
-        orderDetail.setCreatedAt(LocalDateTime.now());
-        orderDetail.setCreatedBy("orderdetail1");
-        //ordergroup 테이블에 group id 1이 있어야 sql 실행됨.
-        orderDetail.setOrderGroupId(1L);
-        //item테이블의 id==1 데이터가 있어야 sql 실행됨.
-        orderDetail.setItemId(1L);
-
+        //어떤 누가?
+        orderDetail.setUserId(1L);
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
-        //Assertions.assertNotNull(newOrderDetail);
 
+        Assertions.assertNotNull(newOrderDetail);
     }
+
 }
